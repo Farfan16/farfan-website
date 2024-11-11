ff98f4edbef34ac01ae562fb44796dc6c4c7be9c
Farrel Fanny <farrelexamples@gmail.com>
2024-11-11 13:58:06 +0700

    add extend to colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "cus-black": "#17153B",
+        "cus-dark-navy": "#2E236C",
+        "cus-light-navy": "#433D8B",
+        "cus-purple": "#C8ACD6",
+      },
+    },
   },
   plugins: [],
 };
